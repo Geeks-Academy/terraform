8a010a8a05cf6a942dd34d5f4ddf2457aff96345
Bartlomiej Wieckowski <wieckb01@stepstone.com>
2020-08-14 11:44:26 +0200

    Add variable.tf and *.tfvars

---
+++ project-apps/irland.tfvars
@@ -0,0 +1,2 @@
+aws_region          = "eu-west-1"
+profile_name        = "irland"
---
+++ project-apps/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "profile_name" {
+  type        = string
+  description = "AWS profile name"
+}
