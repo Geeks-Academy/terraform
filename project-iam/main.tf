02a23219e51855afa34c2e01856ed795ad071fa3
Bartlomiej Wieckowski <wieckb01@stepstone.com>
2020-08-14 15:22:42 +0200

    Allow EC2 to register in ECS

--- project-iam/main.tf
+++ project-iam/main.tf
@@ -39,6 +39,29 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
+resource "aws_iam_policy" "policy" {
+  name        = "EC2-to-ECS"
+  description = ""
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ecs:*",
+            "Resource": "*"
+        }
+    ]
+}
+  POLICY
+}
+
+resource "aws_iam_policy_attachment" "ec2toecs" {
+  name       = "EC2-to-ECS"
+  roles      = ["${aws_iam_role.role.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
+
 output "instance_profile_ec2" {
   value = aws_iam_instance_profile.ec2.name
 }
