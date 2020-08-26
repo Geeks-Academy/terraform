211f52b2bd176e913108696a087b03a882640e26
bwieckow <bar.wieckowski@gmail.com>
2020-08-26 15:49:09 +0200

    POPS-10 - Add user login profile

--- project-iam/users/aleks.tf
+++ project-iam/users/aleks.tf
@@ -1,1 +1,15 @@
 ### TODO create user for Aleks
+resource "aws_iam_user" "aleks" {
+  name          = "Aleks"
+  path          = "/"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "aleks" {
+  user    = aws_iam_user.example.name
+  pgp_key = "keybase:terraform"
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.aleks.encrypted_password
+}
