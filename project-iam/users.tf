1c2d9077a18a70eb699d5b923264cce8bcc02262
bwieckow <bar.wieckowski@gmail.com>
2020-09-02 10:06:27 +0200

    POPS-10 - Create user for ECS services deployment

--- project-iam/users.tf
+++ project-iam/users.tf
@@ -34,3 +34,32 @@ resource "aws_iam_group_policy_attachment" "attachment" {
   group      = aws_iam_group.administrator.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_group" "deployer" {
+  name = "deployer"
+}
+
+resource "aws_iam_group_policy_attachment" "deployer_attachment" {
+  group      = aws_iam_group.deployer.name
+  policy_arn = aws_iam_policy.deployer_policy.arn
+}
+
+resource "aws_iam_policy" "deployer_policy" {
+  name        = "ECS_Deployer"
+  description = ""
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "ecs:*",
+              "ecr:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  POLICY
+}
