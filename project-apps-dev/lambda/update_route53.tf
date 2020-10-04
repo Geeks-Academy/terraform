c265a3a8b71e7b575364371a7f6101ca834a96a8
bwieckow <bar.wieckowski@gmail.com>
2020-10-05 00:06:30 +0200

    POPS-57 - Fix typo

--- project-apps-dev/lambda/update_route53.tf
+++ project-apps-dev/lambda/update_route53.tf
@@ -33,7 +33,7 @@ resource "aws_sns_topic" "asg_events" {
 resource "aws_sns_topic_subscription" "asg_events_sqs_target" {
   topic_arn = aws_sns_topic.asg_events.arn
   protocol  = "lambda"
-  endpoint  = var.lambda_arn
+  endpoint  = aws_lambda_function.update_route53.arn
 }
 
 output "sns_topic_arn" {
