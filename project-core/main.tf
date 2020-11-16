cfa6e5f4d2ca014490219bf02755e20ef543d08b
bwieckow <bar.wieckowski@gmail.com>
2020-11-16 11:08:18 +0100

    POP-63 - Update core network

--- project-core/main.tf
+++ project-core/main.tf
@@ -1,37 +1,41 @@
 terraform {
   backend "s3" {
-    bucket  = "programmers-only-states"
-    region  = "eu-central-1"
-    key     = "states/core/terraform.tfstate"
+    bucket = "programmers-only-states"
+    region = "eu-central-1"
+    key    = "states/core/terraform.tfstate"
   }
 }
 
 data "terraform_remote_state" "project-iam" {
   backend = "s3"
   config = {
-    bucket  = "programmers-only-states"
-    region  = "eu-central-1"
-    key     = "states/iam/terraform.tfstate"
+    bucket = "programmers-only-states"
+    region = "eu-central-1"
+    key    = "states/iam/terraform.tfstate"
   }
 }
 
 module "vpc_common" {
   source = "./vpc"
 
-  owner               = var.owner
-  environment_type    = var.environment_type
-  vpc_cidr            = var.vpc_cidr
-  public_subnets      = var.public_subnets
-  private_subnet_cidr = var.private_subnet_cidr
-  azs                 = var.azs
+  owner            = var.owner
+  environment_type = var.environment_type
+  vpc_cidr         = var.vpc_cidr
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  azs              = var.azs
 }
 
 output "key_name" {
   value = aws_key_pair.deployer.key_name
 }
 
-output "subnets" {
+output "public_subnets" {
   value = module.vpc_common.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc_common.private_subnets
 }
 
 output "vpc_common_id" {
--- project-core/vpc/output.tf
+++ project-core/vpc/output.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "public_subnets" {
   value = aws_subnet.public_subnet.*.id
 }
+
+output "private_subnets" {
+  value = aws_subnet.private_subnet.*.id
+}
