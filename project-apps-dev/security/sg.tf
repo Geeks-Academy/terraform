eda61669134a7fcbb10980eca9dc1b3f10b97913
bwieckow <bar.wieckowski@gmail.com>
2020-12-21 20:28:04 +0100

    POPS-69 - Fix

--- project-apps-dev/security/sg.tf
+++ project-apps-dev/security/sg.tf
@@ -83,15 +83,15 @@ resource "aws_security_group_rule" "rds_allow_psql" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.rds.id
 }
 
-resource "aws_security_group_rule" "alb_egress_all" {
+resource "aws_security_group_rule" "rds_egress_all" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.rds.id
 }
