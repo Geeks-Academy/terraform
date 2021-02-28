2b34111eedd51d68e0c83c962ab6ed5df2014de9
bwieckow <bar.wieckowski@gmail.com>
2021-02-28 18:48:51 +0100

    POPS-78 - Final version of ASG Manager

--- project-apps-dev/asg_manager.py
+++ project-apps-dev/asg_manager.py
@@ -7,9 +7,23 @@ from datetime import date, timedelta
 from calendar import monthrange
 
 def lambda_handler(event, context):
+    UP_DESIRED_CAPACITY = os.environ['UP_COUNT']
+    DOWN_DESIRED_CAPACITY = os.environ['DOWN_COUNT']
     
+    client = boto3.client('autoscaling')
+    
+    if event['type'] == 'UP':
+        DESIRED_CAPACITY = UP_DESIRED_CAPACITY
+    elif event['type'] == 'DOWN':
+        DESIRED_CAPACITY = DOWN_DESIRED_CAPACITY
+    
+    response = client.set_desired_capacity(
+                    AutoScalingGroupName='programmers-only',
+                    DesiredCapacity=DESIRED_CAPACITY,
+                    HonorCooldown=False
+                )
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': response
     }
