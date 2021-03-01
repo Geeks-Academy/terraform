7fe4a743dd205981e44405142ffddf393f4d3333
bwieckow <bar.wieckowski@gmail.com>
2021-03-01 18:45:18 +0100

    POPS-79 - Add DNS entry for geeks.academy

--- project-apps-dev/locals.tf
+++ project-apps-dev/locals.tf
@@ -7,4 +7,8 @@ locals {
     "1" = "www.programmers-only.com"
     "2" = "auth.programmers-only.com"
   }
+
+  public_geeks_academy_dns_entries = {
+    "1" = "www.geeks.academy"
+  }
 }
--- project-apps-dev/public_route53.tf
+++ project-apps-dev/public_route53.tf
@@ -1,3 +1,4 @@
+# Programmers Only
 data "aws_route53_zone" "public" {
   name         = "programmers-only.com."
   private_zone = false
@@ -8,6 +9,23 @@ resource "aws_route53_record" "public" {
   for_each = local.public_dns_entries
 
   zone_id = data.aws_route53_zone.public.zone_id
+  name    = each.value
+  type    = "CNAME"
+  ttl     = "300"
+  records = [module.ProgrammersOnly.alb_dns_name]
+}
+
+# Geeks Academy
+data "aws_route53_zone" "geeks_academy_public" {
+  name         = "geeks.academy."
+  private_zone = false
+}
+
+
+resource "aws_route53_record" "geeks_academy_public" {
+  for_each = local.public_geeks_academy_dns_entries
+
+  zone_id = data.aws_route53_zone.geeks_academy_public.zone_id
   name    = each.value
   type    = "CNAME"
   ttl     = "300"
