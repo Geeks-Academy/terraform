e15bab5a52cfce72dad344a2bd38bb32069312bf
bwieckow <bar.wieckowski@gmail.com>
2021-03-12 00:59:14 +0100

    Add Zabbix Agent config

--- project-apps-dev/userdata.sh
+++ project-apps-dev/userdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "export LOCAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`" >> /etc/profile
 
+export PUBLIC_IP=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
+
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
@@ -12,6 +14,17 @@ service docker restart
 start ecs
 
 yum -y install aws-cli jq
+yum update -y
+yum install -y https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
+yum install -y zabbix-agent vim
+
+sed -i 's/Server=.*/Server=77.55.217.23/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/ServerActive=.*/ServerActive=77.55.217.23/' /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=GeeksAcademyServer-${PUBLIC_IP}" >> /etc/zabbix/zabbix_agentd.conf
+echo "HostnameItem=system.hostname" >> /etc/zabbix/zabbix_agentd.conf
+echo "HostMetadataItem=system.uname" >> /etc/zabbix/zabbix_agentd.conf
+
+systemctl restart zabbix-agent
 
 # echo "
 # #!/bin/bash
