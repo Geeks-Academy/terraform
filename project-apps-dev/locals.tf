c946170eee3a46cb89ff1e5691ef95230254e4ca
Piotr Wachulec <p.wachulec@gmail.com>
2021-04-22 16:30:12 +0200

    Fixed DNS entries

--- project-apps-dev/locals.tf
+++ project-apps-dev/locals.tf
@@ -10,7 +10,6 @@ locals {
 
   public_geeks_academy_dns_entries = {
     "1" = "www.geeks.academy"
-    "2" = "new.geeks.academy"
     "3" = "structure.geeks.academy"
     "4" = "structure-api.geeks.academy"
   }
