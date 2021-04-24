d8e4332710305988830f0ed11f40ed3643c66f73
bwieckow <bar.wieckowski@gmail.com>
2021-04-24 18:41:48 +0200

    Resolve #216 - create group for developers

--- project-iam/users.tf
+++ project-iam/users.tf
@@ -9,68 +9,19 @@ module "Piotr" {
   console_access         = true
 }
 
+# module "Marek" {
+#   source = "../modules/user"
+
+#   username               = "Marek"
+#   group_membership       = "developer"
+#   policy_attachement_arn = module.roles.force_mfa_policy_arn
+#   console_access         = true
+# }
+
 module "ECS_Deployer" {
   source = "../modules/user"
 
   username            = "ecs_deployer"
   group_membership    = "deployer"
   programmatic_access = true
-}
-
-### GROUPS
-
-#ADMINISTRATOR
-resource "aws_iam_group" "administrator" {
-  name = "administrator"
-}
-
-resource "aws_iam_group_policy_attachment" "attachment" {
-  group      = aws_iam_group.administrator.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-#DEPLOYER
-resource "aws_iam_group" "deployer" {
-  name = "deployer"
-}
-
-resource "aws_iam_group_policy_attachment" "deployer_attachment" {
-  group      = aws_iam_group.deployer.name
-  policy_arn = aws_iam_policy.deployer_policy.arn
-}
-
-resource "aws_iam_policy" "deployer_policy" {
-  name        = "ECS_Deployer"
-  description = ""
-  policy      = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-              "ecs:*",
-              "elasticloadbalancing:AddTags",
-              "elasticloadbalancing:CreateTargetGroup",
-              "elasticloadbalancing:DeleteTargetGroup",
-              "elasticloadbalancing:Describe*",
-              "elasticloadbalancing:Modify*",
-              "ecr:*",
-              "iam:PassRole",
-              "sns:List*",
-              "s3:Get*",
-              "s3:List*"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-              "s3:*"
-            ],
-            "Resource": "arn:aws:s3:::trstates/states/apps/dev/*"
-        }
-    ]
-}
-  POLICY
 }
