70832fcb27113f57651ddf7f3f27acbfaa8601c1
bwieckow <bar.wieckowski@gmail.com>
2021-06-20 04:05:31 +0200

    structure_frontend S3 bucket name

--- project-apps-dev/S3/structure_frontend.tf
+++ project-apps-dev/S3/structure_frontend.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "structure_frontend" {
-  bucket = "structure_frontend.geeks.academy"
+  bucket = "structure-frontend.geeks.academy"
   acl    = "public-read"
 
   website {
