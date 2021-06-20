bf9d375393b20bd3a83253b8cf3724035ab47ce4
bwieckow <bar.wieckowski@gmail.com>
2021-06-20 04:23:56 +0200

    Fix forwarded values and restrictions

--- project-apps-dev/cdn.tf
+++ project-apps-dev/cdn.tf
@@ -27,6 +27,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
@@ -45,6 +53,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 31536000
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 
   viewer_certificate {
