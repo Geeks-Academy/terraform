7ac77d3c06181d2b67dd0edf1bb319d413bcf40b
bwieckow <bar.wieckowski@gmail.com>
2021-06-20 03:01:17 +0200

    Create structure_frontend S3 bucket

---
+++ project-apps-dev/S3/output.tf
@@ -0,0 +1,3 @@
+output "structure_frontend_domain_name" {
+  value = aws_s3_bucket.structure_frontend.bucket_regional_domain_name
+}
---
+++ project-apps-dev/S3/structure_frontend.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "structure_frontend" {
+  bucket = "structure_frontend.geeks.academy"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = var.tags
+}
