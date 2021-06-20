22f14ebe4e0310dce5dec3e47a410205d2c224b9
bwieckow <bar.wieckowski@gmail.com>
2021-06-20 04:19:00 +0200

    Create CDN for S3 structure bucket

---
+++ project-apps-dev/cdn.tf
@@ -0,0 +1,56 @@
+data "aws_acm_certificate" "geeks_academy" {
+  domain   = "*.geeks.academy"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = module.S3.structure_frontend_domain_name
+    origin_id   = "S3-structure_frontend.geeks.academy"
+  }
+
+  enabled             = false
+  default_root_object = "index.html"
+
+  aliases = ["*.geeks.academy", "structure.geeks.academy"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-structure_frontend.geeks.academy"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-structure_frontend.geeks.academy"
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  tags = var.tags
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    acm_certificate_arn            = data.aws_acm_certificate.geeks_academy.arn
+  }
+}
