42b8e8636cb3bbf4d0c66606aeb7ef8f90803730
bwieckow <bar.wieckowski@gmail.com>
2021-08-05 00:57:09 +0200

    Fix cdn

--- project-apps-dev/cdn.tf
+++ project-apps-dev/cdn.tf
@@ -33,6 +33,18 @@ resource "aws_cloudfront_distribution" "structure" {
 
   aliases = ["structure.geeks.academy"]
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+  
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
   default_cache_behavior {
     allowed_methods          = ["GET", "HEAD"]
     cached_methods           = ["GET", "HEAD"]
