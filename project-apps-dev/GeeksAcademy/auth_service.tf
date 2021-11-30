953d1104cada4bbf0b649219c1fc6451e2edffdb
bwieckow <bar.wieckowski@gmail.com>
2021-11-30 20:47:06 +0100

    Migration

--- project-apps-dev/GeeksAcademy/auth_service.tf
+++ project-apps-dev/GeeksAcademy/auth_service.tf
@@ -1,153 +1,153 @@
-### ECS SERVICES
+# ### ECS SERVICES
 
-data "template_file" "auth" {
-  template = file("GeeksAcademy/task_definitions/auth_task_definition.json")
-}
+# data "template_file" "auth" {
+#   template = file("GeeksAcademy/task_definitions/auth_task_definition.json")
+# }
 
-resource "aws_ecs_task_definition" "auth" {
-  family                = "auth"
-  container_definitions = file("GeeksAcademy/task_definitions/auth_task_definition.json")
-  task_role_arn         = aws_iam_role.ecs_role.arn
-  execution_role_arn    = aws_iam_role.ecs_role.arn
-}
+# resource "aws_ecs_task_definition" "auth" {
+#   family                = "auth"
+#   container_definitions = file("GeeksAcademy/task_definitions/auth_task_definition.json")
+#   task_role_arn         = aws_iam_role.ecs_role.arn
+#   execution_role_arn    = aws_iam_role.ecs_role.arn
+# }
 
-data "aws_ecs_task_definition" "auth" {
-  task_definition = aws_ecs_task_definition.auth.family
-  depends_on      = [aws_ecs_task_definition.auth]
-}
+# data "aws_ecs_task_definition" "auth" {
+#   task_definition = aws_ecs_task_definition.auth.family
+#   depends_on      = [aws_ecs_task_definition.auth]
+# }
 
-resource "aws_ecr_repository" "auth" {
-  name = "auth"
-}
+# resource "aws_ecr_repository" "auth" {
+#   name = "auth"
+# }
 
-resource "aws_ecr_repository_policy" "auth" {
-  repository = aws_ecr_repository.auth.name
+# resource "aws_ecr_repository_policy" "auth" {
+#   repository = aws_ecr_repository.auth.name
 
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "auth_ECR",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy"
-            ]
-        }
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#     "Version": "2008-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "auth_ECR",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": [
+#                 "ecr:GetDownloadUrlForLayer",
+#                 "ecr:BatchGetImage",
+#                 "ecr:BatchCheckLayerAvailability",
+#                 "ecr:PutImage",
+#                 "ecr:InitiateLayerUpload",
+#                 "ecr:UploadLayerPart",
+#                 "ecr:CompleteLayerUpload",
+#                 "ecr:DescribeRepositories",
+#                 "ecr:GetRepositoryPolicy",
+#                 "ecr:ListImages",
+#                 "ecr:DeleteRepository",
+#                 "ecr:BatchDeleteImage",
+#                 "ecr:SetRepositoryPolicy",
+#                 "ecr:DeleteRepositoryPolicy"
+#             ]
+#         }
+#     ]
+# }
+# EOF
+# }
 
-resource "aws_ecr_lifecycle_policy" "auth" {
-  repository = aws_ecr_repository.auth.name
+# resource "aws_ecr_lifecycle_policy" "auth" {
+#   repository = aws_ecr_repository.auth.name
 
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last 5 images",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "imageCountMoreThan",
-                "countNumber": 5
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#     "rules": [
+#         {
+#             "rulePriority": 1,
+#             "description": "Keep last 5 images",
+#             "selection": {
+#                 "tagStatus": "untagged",
+#                 "countType": "imageCountMoreThan",
+#                 "countNumber": 5
+#             },
+#             "action": {
+#                 "type": "expire"
+#             }
+#         }
+#     ]
+# }
+# EOF
+# }
 
-resource "aws_ecs_service" "auth" {
-  name                               = "auth"
-  cluster                            = aws_ecs_cluster.geeks_academy.id
-  task_definition                    = "auth:${data.aws_ecs_task_definition.auth.revision}"
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 0
+# resource "aws_ecs_service" "auth" {
+#   name                               = "auth"
+#   cluster                            = aws_ecs_cluster.geeks_academy.id
+#   task_definition                    = "auth:${data.aws_ecs_task_definition.auth.revision}"
+#   desired_count                      = 1
+#   deployment_minimum_healthy_percent = 0
 
-  load_balancer {
-    target_group_arn = aws_alb_target_group.auth.arn
-    container_name   = "auth"
-    container_port   = 3000
-  }
-}
+#   load_balancer {
+#     target_group_arn = aws_alb_target_group.auth.arn
+#     container_name   = "auth"
+#     container_port   = 3000
+#   }
+# }
 
-resource "aws_alb_target_group" "auth" {
-  name_prefix = "po-"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+# resource "aws_alb_target_group" "auth" {
+#   name_prefix = "po-"
+#   port        = 3000
+#   protocol    = "HTTP"
+#   vpc_id      = var.vpc_id
 
-  health_check {
-    path     = "/"
-    port     = "traffic-port"
-    interval = 300
-    matcher  = "200-499"
-  }
+#   health_check {
+#     path     = "/"
+#     port     = "traffic-port"
+#     interval = 300
+#     matcher  = "200-499"
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-### ENV PASSING
+# ### ENV PASSING
 
-resource "aws_iam_role" "ecs_role" {
-  name               = "ecs_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+# resource "aws_iam_role" "ecs_role" {
+#   name               = "ecs_role"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "ecs-tasks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy" "ecs_service_role_policy" {
-  name     = "ecs_service_role_policy"
-  role     = aws_iam_role.ecs_role.id
-  policy   = <<-EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ecr:*",
-        "ssm:GetParameters",
-        "ssm:GetParameter",
-        "ssm:DescribeParameters",
-        "secretsmanager:GetSecretValue"
-      ],
-      "Resource": [
-        "*"
-      ]
-    }
-  ]
-}
-EOF
-}
+# resource "aws_iam_role_policy" "ecs_service_role_policy" {
+#   name     = "ecs_service_role_policy"
+#   role     = aws_iam_role.ecs_role.id
+#   policy   = <<-EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "ecr:*",
+#         "ssm:GetParameters",
+#         "ssm:GetParameter",
+#         "ssm:DescribeParameters",
+#         "secretsmanager:GetSecretValue"
+#       ],
+#       "Resource": [
+#         "*"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
